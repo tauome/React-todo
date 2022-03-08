2bf57f3e959815d224d9a0011b20ce1c23be4325
Solaiman <tausif.solaiman@accenture.com>
2022-03-08 14:43:22 +0000

    components seperated

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
 
-function TodoForm ({addTodo}) {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-
-    if(!value) return;
-    addTodo(value);
-    setValue(""); 
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-      type='text'
-      className='input'
-      value={value}
-      onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-};
-
-function Todo ({ todo, index, completeTodo, removeTodo }) {
-  return (
-    <div 
-    className="todo"
-    style={{textDecoration: todo.isCompleted? "line-through": ""}}
-    >
-      {todo.text}
-      <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>x</button>
-      </div>
-    </div>
-  );
-};
 
 function App () {
 
